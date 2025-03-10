5d6723109497335015239bdccfa9d699d4069c01
Shri Om Trivedi <tshriom@gmail.com>
2025-03-10 22:27:14 +0530

    characters added, sprite added

--- game/main.go
+++ game/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"image"
 	"image/color"
 	"log"
 
@@ -8,15 +10,146 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+type Sprite struct {
+	Img  *ebiten.Image
+	X, Y float64
+}
+
+type Enemy struct {
+	*Sprite
+	FollowsPlayer bool
+}
+
+type Potion struct {
+	*Sprite
+	AmtHeal uint
+}
+
+type Player struct {
+	*Sprite
+	Health uint
+}
+
+type Game struct {
+	player  *Player
+	enemies []*Enemy
+	potions []*Potion
+}
 
 func (g *Game) Update() error {
+
+	if ebiten.IsKeyPressed(ebiten.KeyD) || ebiten.IsKeyPressed(ebiten.KeyRight) {
+		g.player.X += 2
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyA) || ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		g.player.X -= 2
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyW) || ebiten.IsKeyPressed(ebiten.KeyUp) {
+		g.player.Y -= 2
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyS) || ebiten.IsKeyPressed(ebiten.KeyDown) {
+		g.player.Y += 2
+	}
+
+	for _, enemy := range g.enemies {
+		if enemy.FollowsPlayer {
+			if enemy.X < g.player.X {
+				enemy.X += 1
+			} else if enemy.X > g.player.X {
+				enemy.X -= 1
+			}
+
+			if enemy.Y < g.player.Y {
+				enemy.Y += 1
+			} else if enemy.Y > g.player.Y {
+				enemy.Y -= 1
+			}
+		}
+	}
+
+	for _, potion := range g.potions{
+		if g.player.X > potion.X {
+			g.player.Health += potion.AmtHeal
+			fmt.Printf("Picked up potion! Health: %d\n", g.player.Health)
+		}
+	}
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.RGBA{120, 180, 255, 255})
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+
+	op := &ebiten.DrawImageOptions{}
+
+	// Scale factors (reduce size)
+	scaleX := 0.3 // Shrinks width to 50%
+	scaleY := 0.3 // Shrinks height to 50%
+
+	// Apply scaling
+	op.GeoM.Scale(scaleX, scaleY)
+
+	// Track positions
+	op.GeoM.Translate(g.player.X, g.player.Y)
+
+	// Draw our player
+	screen.DrawImage(
+		g.player.Img.SubImage(
+			image.Rect(0, 0, 150, 150),
+		).(*ebiten.Image),
+		op,
+	)
+
+	op.GeoM.Reset()
+
+	// Draw enemies (goblins)
+	for _, enemy := range g.enemies {
+
+		// Scale factors (reduce size)
+		scaleX := 0.3 // Shrinks width to 50%
+		scaleY := 0.3 // Shrinks height to 50%
+
+		// Apply scaling
+		op.GeoM.Scale(scaleX, scaleY)
+
+		op.GeoM.Translate(enemy.X, enemy.Y)
+
+		screen.DrawImage(
+			enemy.Img.SubImage(
+				image.Rect(0, 0, 150, 150),
+			).(*ebiten.Image),
+			op,
+		)
+
+		op.GeoM.Reset()
+
+	}
+
+	// Draw potions
+	for _, enemy := range g.potions {
+
+		// Scale factors (reduce size)
+		scaleX := 0.3 // Shrinks width to 50%
+		scaleY := 0.3 // Shrinks height to 50%
+
+		// Apply scaling
+		op.GeoM.Scale(scaleX, scaleY)
+
+		op.GeoM.Translate(enemy.X, enemy.Y)
+
+		screen.DrawImage(
+			enemy.Img.SubImage(
+				image.Rect(0, 0, 150, 150),
+			).(*ebiten.Image),
+			op,
+		)
+
+		op.GeoM.Reset()
+
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -26,8 +159,66 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+
+	playerImg, _, err := ebitenutil.NewImageFromFile("../assets/images/warrior-main.png")
+	if err != nil {
+		// handle error
+		log.Fatal(err)
+	}
+
+	goblinFireImg, _, err := ebitenutil.NewImageFromFile("../assets/images/goblin_fire.png")
+	if err != nil {
+		// handle error
+		log.Fatal(err)
+	}
+	
+	potionImg, _, err := ebitenutil.NewImageFromFile("../assets/images/meat.png")
+	if err != nil {
+		// handle error
+		log.Fatal(err)
+	}
+
+	game := Game{
+		player: &Player{
+			Sprite: &Sprite {
+				Img: playerImg,
+				X:   50,
+				Y:   50,
+			},
+			Health: 5,
+		},
+			
+		enemies: []*Enemy{
+			{
+				&Sprite{
+					Img: goblinFireImg,
+					X:   150,
+					Y:   150,
+				},
+				true,
+			},
+			{
+				&Sprite{
+					Img: goblinFireImg,
+					X:   150,
+					Y:   100,
+				},
+				false,
+			},
+		},
+		potions: []*Potion{
+			{
+				&Sprite{
+					Img: potionImg,
+					X:   120,
+					Y:   120,
+				},
+				5,
+			},
+		},
+	}
+
+	if err := ebiten.RunGame(&game); err != nil {
 		log.Fatal(err)
 	}
 }
